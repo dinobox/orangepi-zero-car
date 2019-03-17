bf2408091b5d32eaa78723df1fdc7a3f25980e95
cr4fun <cr4fun@outlook.com>
2019-03-17 14:37:00 +0800

    up

--- public/app.js
+++ public/app.js
@@ -1,21 +1,35 @@
 $(document).ready(function () {
-    $("#up").click(function () {
-        console.log("up");
-    });
-    $("#down").click(function () {
-        console.log("down");
-    });
-    $("#left").click(function () {
-        console.log("left");
-    });
-    $("#right").click(function () {
-        console.log("right");
-    });
-    $("#stop").click(function () {
-        console.log("stop");
+    function stop(){
         $.get("/3/0");
         $.get("/15/0");
         $.get("/16/0");
         $.get("/14/0");
+    }
+    $("#up").click(function () {
+        console.log("up");
+        stop();
+        $.get("/15/1");
+        $.get("/16/1");
+    });
+    $("#down").click(function () {
+        console.log("down");
+        stop();
+        $.get("/3/1");
+        $.get("/14/1");
+
+    });
+    $("#left").click(function () {
+        console.log("left");
+        stop();
+        $.get("/15/1");
+    });
+    $("#right").click(function () {
+        console.log("right");
+        $.get("/16/1");
+        stop();
+    });
+    $("#stop").click(function () {
+        console.log("stop");
+        stop();
     });
 });
